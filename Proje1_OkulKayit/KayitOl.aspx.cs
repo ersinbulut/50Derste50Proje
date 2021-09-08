2b620cf7c09dd7992db8a57e5754b8af85294d4d
ersinbulut <ersinblt61@gmail.com>
2021-09-08 14:49:20 +0300

    Add project files.

---
+++ Proje1_OkulKayit/KayitOl.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows;
+
+namespace Proje1_OkulKayit
+{
+    public partial class KayitOl : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.TBLOGRENCİTableAdapter dt = new DataSet1TableAdapters.TBLOGRENCİTableAdapter();
+            dt.OgrenciEkle(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text);
+            
+        }
+
+    }
+}
