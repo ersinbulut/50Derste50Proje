41d1aebff9f89b9e3d8c7a2a15b8d9895d8416b2
ersinbulut <ersinblt61@gmail.com>
2021-09-08 14:58:50 +0300

    Muzik Okulu Kayıt projesi

---
+++ Proje2_OgrenciKayit/Proje2_OgrenciKayit/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Proje2_OgrenciKayit
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.TBLOGRENCİLERTableAdapter dt = new DataSet1TableAdapters.TBLOGRENCİLERTableAdapter();
+            dt.OgrenciEkle(TxtAdSoyad.Text, TxtNumara.Text, TxtMail.Text, DropDownList1.SelectedItem.ToString(), DropDownList2.SelectedItem.ToString());
+        }
+    }
+}
